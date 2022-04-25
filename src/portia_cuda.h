96d66183cfdde655cb80cc480599f32353015053
Pedro S. <pedromiguelfilipesimoes@proton.me>
2022-04-26 00:52:10 +0200

    Added default block size
    Signed-off-by: GitHub <noreply@github.com>

--- src/portia_cuda.h
+++ src/portia_cuda.h
@@ -22,6 +22,9 @@ Copyright (C) 2022 Pedro Simões
             }\
         }
 
+// Default block size for kernel executions.
+#define BLOCK_SIZE 256
+
 // Util functions:
 
 /// Marsiglia's xorshift pseudo-random number generator with period 2^32-1.
