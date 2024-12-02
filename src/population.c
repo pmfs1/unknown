01c6014b48ce5e3827b225f0129678c9625f8c1e
Pedro S. <pedromiguelfilipesimoes@proton.me>
2024-12-02 10:40:46 +0000

    2024.12.02.1039 (#17)
    _This pull request includes several changes to the `src/cortex.c` and
    `src/population.c` files. The changes primarily involve the removal of
    certain code blocks and the addition of TODO comments to indicate where
    further implementation is needed._
    
    *
    _[`src/cortex.c`](diffhunk://#diff-7b2e8cd347742efb0878ea75d5257af9bab2af1f4956d00a62086fb4a2dc5f5eL467-R467):
    Removed the code that sets the wrapping state in the `c2d_set_wrapped`
    function and replaced it with a TODO comment._
    *
    _[`src/population.c`](diffhunk://#diff-7b956a5b442cdc9b942f4456c29bab9ae0c4c84b2447bdcfae52f99ca8c6a713L239-R239):
    Removed the code that sets the recovery value, excitatory value, and
    decay value in the `p2d_breed` function and replaced it with a TODO
    comment._
    *
    _[`src/population.c`](diffhunk://#diff-7b956a5b442cdc9b942f4456c29bab9ae0c4c84b2447bdcfae52f99ca8c6a713L261-R248):
    Removed the code that sets the maximum total strength in the `p2d_breed`
    function and replaced it with a TODO comment._

--- src/population.c
+++ src/population.c
@@ -236,20 +236,7 @@ void p2d_breed(unk_population2d_t *population, unk_cortex2d_t **child)
     population->rand_state = xorshf32(population->rand_state);
     winner_parent_index = population->rand_state % population->parents_count;
     c2d_set_fire_threshold(*child, parents[winner_parent_index].fire_threshold);
-    //// FROM HERE
-    // PICK RECOVERY VALUE FROM A RANDOM PARENT
-    population->rand_state = xorshf32(population->rand_state);
-    winner_parent_index = population->rand_state % population->parents_count;
-    (*child)->recovery_value = parents[winner_parent_index].recovery_value;
-    // PICK EXCITATORY VALUE FROM A RANDOM PARENT
-    population->rand_state = xorshf32(population->rand_state);
-    winner_parent_index = population->rand_state % population->parents_count;
-    (*child)->exc_value = parents[winner_parent_index].exc_value;
-    // PICK DECAY VALUE FROM A RANDOM PARENT
-    population->rand_state = xorshf32(population->rand_state);
-    winner_parent_index = population->rand_state % population->parents_count;
-    (*child)->decay_value = parents[winner_parent_index].decay_value;
-    //// TO HERE. A LOGICAL CHECK IS NEEDED
+    // [TODO] SET RECOVERY VALUE AND EXC/DECAY VALUES
     // PICK SYNGEN CHANCE FROM A RANDOM PARENT
     population->rand_state = xorshf32(population->rand_state);
     winner_parent_index = population->rand_state % population->parents_count;
@@ -258,10 +245,7 @@ void p2d_breed(unk_population2d_t *population, unk_cortex2d_t **child)
     population->rand_state = xorshf32(population->rand_state);
     winner_parent_index = population->rand_state % population->parents_count;
     c2d_set_synstr_chance(*child, parents[winner_parent_index].synstr_chance);
-    // SET MAX TOTAL STRENGTH FROM A RANDOM PARENT
-    population->rand_state = xorshf32(population->rand_state);
-    winner_parent_index = population->rand_state % population->parents_count;
-    (*child)->max_tot_strength = parents[winner_parent_index].max_tot_strength;
+    // [TODO] SET MAX TOT STRENGTH
     // PICK MAX SYN COUNT FROM A RANDOM PARENT
     population->rand_state = xorshf32(population->rand_state);
     winner_parent_index = population->rand_state % population->parents_count;
